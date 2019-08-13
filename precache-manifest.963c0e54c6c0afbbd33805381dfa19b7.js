4bd8edd4ee4ddadb7aead19f150c23088be44504
Apan Singhal <apan08@gmail.com>
2019-08-13 19:18:22 +0530

    Updates

---
+++ precache-manifest.963c0e54c6c0afbbd33805381dfa19b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166e18a48757b2db343ee72c9df19262",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "1222c138ba1b99ac6c10",
+    "url": "/mosters-rolodex/static/css/main.2be3784b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/mosters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "1222c138ba1b99ac6c10",
+    "url": "/mosters-rolodex/static/js/main.59559673.chunk.js"
+  },
+  {
+    "revision": "253f6367d1a1bea6b3cd",
+    "url": "/mosters-rolodex/static/js/runtime~main.7c72627f.js"
+  }
+]);
